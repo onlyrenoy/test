e6d3a8401bf708547820e2a647a7de32b900c765
Administrator <Administrator@10.16.4.118>
2018-05-08 10:31:01 +0300

    test

---
+++ 01Configuration/JavaConfiguration/src/main/java/com/oreilly/sdata/Application.java
@@ -0,0 +1,24 @@
+package com.oreilly.sdata;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.apache.commons.logging.Log;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		try(AnnotationConfigApplicationContext context 
+				= new AnnotationConfigApplicationContext(DataConfiguration.class)){
+				BookService service = context.getBean(BookService.class);
+				Book book = new Book("First Book", new Date(), 33, new BigDecimal("26.00"));
+				service.save(book);
+				System.out.println("saved");
+			}
+		
+	}
+}
---
+++ 01Configuration/SpringBootConfiguration/src/main/java/com/oreilly/sdata/data/entities/Book.java
@@ -0,0 +1,69 @@
+package com.oreilly.sdata.data.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="BOOK")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long bookId;
+	
+	private String title;
+	
+	private Date publishDate;
+	
+	private int pageCount;
+	
+	private BigDecimal price;
+
+	public Long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Long bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+	
+}
